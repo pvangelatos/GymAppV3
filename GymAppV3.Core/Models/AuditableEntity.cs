4b91a805ea60f7e3227f01483646b74f1d3529a6
pvangelatos <p.vangelatos23@hotmail.com>
2026-07-08 00:41:17 +0300

    GymAppV3: initial Core + Infrastructure (models, mappings, DbContext)

---
+++ GymAppV3.Core/Models/AuditableEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymAppV3.Core.Models
+{
+    public class AuditableEntity
+    {
+        public string? CreatedBy { get; set; }
+        public DateTimeOffset CreatedAt { get; set; } = DateTime.UtcNow;
+        public string? UpdatedBy { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ GymAppV3.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using GymAppV3.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymAppV3.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MembershipPackage> MembershipPackages => Set<MembershipPackage>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
