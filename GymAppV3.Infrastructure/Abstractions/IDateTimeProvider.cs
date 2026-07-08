ab9234bf7538e27fe5bb29280bb5b7f0c072bfb9
pvangelatos <p.vangelatos23@hotmail.com>
2026-07-08 22:56:53 +0300

    Add core infrastructure interfaces and update solution items

---
+++ GymAppV3.Infrastructure/Abstractions/IDateTimeProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymAppV3.Infrastructure.Abstractions
+{
+    public interface IDateTimeProvider
+    {
+        DateTimeOffset UtcNow { get; }
+    }
+}
---
+++ GymAppV3.Infrastructure/Abstractions/IUserContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymAppV3.Infrastructure.Abstractions
+{
+    public interface IUserContext
+    {
+        string? UserId { get; }
+    }
+}
